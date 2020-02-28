6ca7989bc1fc705f9feeef904f2df74df9b0a021
Tal Koren <talkor@users.noreply.github.com>
2020-02-28 20:50:43 +0200

    add composition api

---
+++ src/components/Button.vue
@@ -0,0 +1,15 @@
+<template></template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  data() {
+    return {
+      msg: "Welcome to Boilr"
+    };
+  }
+};
+</script>
+
+<style scoped lang="scss">
+</style>
