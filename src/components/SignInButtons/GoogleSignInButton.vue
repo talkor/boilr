a43a6eb5c598727051c7b9618c34ef1fddd7538b
Tal Koren <talkor@users.noreply.github.com>
2020-05-01 23:34:01 +0300

    refactor register + sign in process, add button, redsign views

---
+++ src/components/SignInButtons/GoogleSignInButton.vue
@@ -0,0 +1,49 @@
+<template>
+  <Button
+    class="google-button"
+    rounded
+    @onClick="onLogin"
+    size="is-medium"
+    text="Sign In With Google"
+  >
+    <img src="../../assets/google.webp" class="google-icon" />
+  </Button>
+</template>
+
+<script>
+import * as firebase from 'firebase/app';
+import 'firebase/auth';
+import Button from '@/components/core/Button/Button';
+
+export default {
+  setup(props, { root }) {
+    const router = root.$router;
+
+    const onLogin = async () => {
+      try {
+        const googleProvider = new firebase.auth.GoogleAuthProvider();
+        await firebase.auth().signInWithPopup(googleProvider);
+        router.replace({ name: 'Home' });
+      } catch (error) {
+        throw new Error(error);
+      }
+    };
+
+    return {
+      onLogin
+    };
+  },
+  components: { Button }
+};
+</script>
+<style scoped lang="scss">
+.google-button {
+  width: 250px;
+  margin: 10px 0;
+
+  .google-icon {
+    width: 20px;
+    margin-right: 10px;
+  }
+}
+</style>
