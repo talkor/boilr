a6951bf5e29c375d209d7c6a90c78950b4133690
Tal Koren <talkor@users.noreply.github.com>
2020-05-01 19:29:31 +0300

    add login and register views, adjust router

---
+++ src/components/core/Button/Button.vue
@@ -0,0 +1,23 @@
+<template>
+  <b-button rounded @click="onClick">{{ text }}</b-button>
+</template>
+
+<script>
+export default {
+  name: 'Button',
+  props: {
+    text: {
+      type: String
+    }
+  },
+  setup(props, { emit }) {
+    const onClick = event => {
+      emit('onClick', event);
+    };
+
+    return { onClick };
+  }
+};
+</script>
+
+<style scoped lang="scss"></style>
