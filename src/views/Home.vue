e990a837eb294a03270eaff206f6a8dc776c2d9e
Tal Koren <talkor@users.noreply.github.com>
2020-05-01 23:33:21 +0300

    clear home view

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,29 +1,5 @@
 <template>
   <div class="home">
-    <router-link to="login">Go To Login</router-link>
-    <button @click="onLogOut">LogOut</button>
+    <h1>Welcome!</h1>
   </div>
 </template>
-
-<script>
-import * as firebase from 'firebase/app';
-import 'firebase/auth';
-
-export default {
-  setup(props, { root }) {
-    const router = root.$router;
-
-    const onLogOut = async () => {
-      try {
-        await firebase.auth().signOut();
-        router.replace({ name: 'Login' });
-      } catch (error) {
-        throw new Error(error);
-      }
-    };
-    return {
-      onLogOut
-    };
-  }
-};
-</script>
