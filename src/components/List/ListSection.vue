21f9fb9895ec9af3564a0d4d3b3e277287abe5a5
Tal Koren <talkor@users.noreply.github.com>
2020-05-02 19:58:56 +0300

    add List component + settings view

---
+++ src/components/List/ListSection.vue
@@ -0,0 +1,35 @@
+<template>
+  <Fragment>
+    <b-menu-list :label="label">
+      <ListItem
+        v-for="(item, index) in items"
+        :key="index"
+        :label="item.label"
+        :icon="item.icon"
+        :action="item.action"
+      />
+    </b-menu-list>
+  </Fragment>
+</template>
+
+<script>
+import { Fragment } from 'vue-fragment';
+import ListItem from './ListItem';
+
+export default {
+  props: {
+    items: {
+      type: Array
+    },
+    label: {
+      type: String
+    }
+  },
+  components: {
+    ListItem,
+    Fragment
+  }
+};
+</script>
+
+<style scoped lang="scss"></style>
