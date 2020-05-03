2639643940eb9ce274959591091ee684807ffd3d
Tal Koren <talkor@users.noreply.github.com>
2020-05-03 22:53:28 +0300

    test qr scanner

--- src/views/Device.vue
+++ src/views/Device.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="device">
-    <qrcode-stream @decode="onDecode"></qrcode-stream>
+    <qrcode-stream
+      class="qr-code"
+      v-if="showDecode"
+      @decode="onDecode"
+    ></qrcode-stream>
     <div>{{ message }}</div>
   </div>
 </template>
@@ -11,15 +15,31 @@ import { QrcodeStream } from 'vue-qrcode-reader';
 
 export default {
   setup() {
-    const message = ref('');
+    const message = ref('Scanning...');
+    const showDecode = ref(true);
 
     const onDecode = decodedString => {
       window.navigator.vibrate(200);
       message.value = decodedString;
+      showDecode.value = false;
     };
 
-    return { onDecode, message };
+    return {
+      onDecode,
+      message,
+      showDecode
+    };
   },
-  components: { QrcodeStream }
+  components: {
+    QrcodeStream
+  }
 };
 </script>
+
+<style scoped lang="scss">
+.qr-code {
+  width: 300px;
+  height: 300px;
+  border: 4px dashed black;
+}
+</style>
