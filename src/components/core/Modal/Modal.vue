5486eeaf0b7537e5846ad9c65f949629c19336f3
Tal Koren <talkor@users.noreply.github.com>
2020-07-18 14:16:12 +0300

    add Modal component

---
+++ src/components/core/Modal/Modal.vue
@@ -0,0 +1,31 @@
+<template>
+  <b-modal aria-role="dialog" aria-modal :active.sync="isActive">
+    <div class="modal-card" style="width: auto">
+      <header class="modal-card-head">
+        <p class="modal-card-title">Edit time</p>
+      </header>
+      <section class="modal-card-body">
+        <slot></slot>
+      </section>
+    </div>
+    <footer class="modal-card-foot">
+      <Button :noBorder="true" @click="$parent.close()" text="Cancel" />
+      <Button :primary="true" text="Save" />
+    </footer>
+  </b-modal>
+</template>
+
+<script>
+import Button from '@/components/core/Button/Button';
+
+export default {
+  props: {
+    isActive: {
+      type: Boolean
+    }
+  },
+  components: {
+    Button
+  }
+};
+</script>
