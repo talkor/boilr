e218906045d8d093243f52e1b93ac02e55b362a4
Tal Koren <talkor@users.noreply.github.com>
2020-07-31 21:08:03 +0300

    be able to use demo when not authenticated

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,5 +1,9 @@
 import * as firebase from 'firebase/app';
 
 export const getUserUid = () => {
+  if (!firebase.auth().currentUser) {
+    return null;
+  }
+
   return firebase.auth().currentUser.uid;
 };
--- src/services/userService.js
+++ src/services/userService.js
@@ -3,5 +3,8 @@ import { getData } from '@/services/databaseService';
 
 export const getUserData = async () => {
   const userUid = await getUserUid();
+  if (!userUid) {
+    return;
+  }
   return getData({ collection: 'users', doc: userUid });
 };
