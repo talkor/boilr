25f3777779d3f51cff640ca91a060e10a9610f9e
Tal Koren <talkor@users.noreply.github.com>
2020-08-07 16:37:51 +0300

    add netlify function

---
+++ functions/func.js
@@ -0,0 +1,23 @@
+const admin = require('firebase-admin');
+const admin = require('firebase-admin');
+const serviceAccount = require('path/to/serviceAccountKey.json');
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: 'https://boilr-app.firebaseio.com'
+});
+
+const db = admin.firestore();
+
+exports.handler = async (event, context, callback) => {
+  await db.collection('users').add({
+    name: 'Test'
+  });
+
+  return callback(null, {
+    statusCode: 200,
+    body: JSON.stringify({
+      data: `Test data added successfully`
+    })
+  });
+};
