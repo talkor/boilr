c77fe8eec02c40836270f4d701c04f99d59b206b
Tal Koren <talkor@users.noreply.github.com>
2020-08-07 17:20:01 +0300

    fix function

--- functions/func.js
+++ functions/func.js
@@ -1,5 +1,5 @@
 const admin = require('firebase-admin');
-const serviceAccount = require('path/to/serviceAccountKey.json');
+const serviceAccount = require('./serviceAccountKey.json');
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
