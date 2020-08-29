01fd321061f584d28babae18907771808d8aeb15
Tal Koren <talkor@users.noreply.github.com>
2020-08-29 17:42:15 +0300

    try to fix ios

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,9 +1,3 @@
 importScripts('https://www.gstatic.com/firebasejs/5.5.6/firebase-app.js');
 importScripts('https://www.gstatic.com/firebasejs/5.5.6/firebase-messaging.js');
 
-firebase.initializeApp({
-  messagingSenderId: '709372845693'
-});
-
-const messaging = firebase.messaging();
-
