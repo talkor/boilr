95bc7460c482431fb2c9b240b1a3a7e6c5f6c8b1
Daniel Cohen <danielc7777@gmail.com>
2020-08-29 11:46:53 +0300

    adding label component

---
+++ src/components/core/Label.vue
@@ -0,0 +1,20 @@
+<template>
+  <h2 class="label">{{ text }}</h2>
+</template>
+
+<script>
+export default {
+  props: {
+    text: String
+  },
+  setup() {}
+};
+</script>
+
+<style scoped lang="scss">
+h2.label {
+  margin-block-end: 0;
+  font-size: 15px;
+  text-align: center;
+}
+</style>
