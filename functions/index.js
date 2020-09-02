894c102f59c1d4af7202e7a061bd0745adcb4595
Tal Koren <talkor@users.noreply.github.com>
2020-09-02 23:01:50 +0300

    Server | map times and turn on/off automatically

--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,7 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 const spacetime = require('spacetime');
+const { schedule } = require('firebase-functions/lib/providers/pubsub');
 admin.initializeApp();
 
 const db = admin.firestore();
@@ -12,28 +13,109 @@ exports.taskRunner = functions.pubsub
   .schedule('* * * * *')
   .timeZone('Asia/Jerusalem')
   .onRun(async (context) => {
-    console.log('TIME = ', spacetime.now().goto('Asia/Jerusalem'));
-    const { active, temperature } = await db
-      .collection('devices')
-      .doc('mhXWbGB4UxIdOPqeoOJz')
-      .get()
-      .then((snapshot) => {
-        return { ...snapshot.data() };
-      });
+    const schedule = await fetchSchedule();
+    scheduler(schedule);
 
-    if (active) {
-      if (temperature < MAX_TEMP) {
-        db.collection('devices')
-          .doc('mhXWbGB4UxIdOPqeoOJz')
-          .set({ temperature: temperature + 2 }, { merge: true });
-      } else {
-        db.collection('devices')
-          .doc('mhXWbGB4UxIdOPqeoOJz')
-          .set({ active: false }, { merge: true });
+    // Simulation
+    temperatueSimulation();
+  });
+
+const getCurrentDay = () => {
+  return spacetime.now().goto('Asia/Jerusalem').day();
+};
+
+const getCurrentTime = () => {
+  return spacetime.now().goto('Asia/Jerusalem').format('time-24');
+};
+
+const fetchSchedule = async () => {
+  const { schedule } = await db
+    .collection('devices')
+    .doc('mhXWbGB4UxIdOPqeoOJz')
+    .get()
+    .then((snapshot) => {
+      return { ...snapshot.data() };
+    });
+
+  return schedule;
+};
+
+const temperatueSimulation = async () => {
+  const { active, temperature } = await db
+    .collection('devices')
+    .doc('mhXWbGB4UxIdOPqeoOJz')
+    .get()
+    .then((snapshot) => {
+      return { ...snapshot.data() };
+    });
+
+  if (active) {
+    if (temperature < MAX_TEMP) {
+      setBoilerTemperature(temperature + 2);
+    } else {
+      setBoilerActive(false);
+    }
+  } else if (temperature > MIN_TEMP) {
+    setBoilerTemperature(temperature - 0.1);
+  }
+};
+
+const setBoilerActive = async (value) => {
+  return db
+    .collection('devices')
+    .doc('mhXWbGB4UxIdOPqeoOJz')
+    .set({ active: value }, { merge: true });
+};
+
+const getBoilerActive = async () => {
+  const { active } = await db
+    .collection('devices')
+    .doc('mhXWbGB4UxIdOPqeoOJz')
+    .get()
+    .then((snapshot) => {
+      return { ...snapshot.data() };
+    });
+
+  return active;
+};
+
+const setBoilerTemperature = async (value) => {
+  return db
+    .collection('devices')
+    .doc('mhXWbGB4UxIdOPqeoOJz')
+    .set({ temperature: value }, { merge: true });
+};
+
+const timeMapper = (schedule) => {
+  let timeMapper = { 0: [], 1: [], 2: [], 3: [], 4: [], 5: [], 6: [] };
+
+  schedule.map((item) => {
+    item.days.forEach((day, dayIndex) => {
+      if (day) {
+        const boilerStartTime = spacetime()
+          .time(item.time)
+          .subtract(item.duration, 'minutes')
+          .format('time-24');
+        timeMapper[dayIndex].push({ time: boilerStartTime, event: 'on' });
+        timeMapper[dayIndex].push({ time: item.time, event: 'off' });
       }
-    } else if (temperature > MIN_TEMP) {
-      db.collection('devices')
-        .doc('mhXWbGB4UxIdOPqeoOJz')
-        .set({ temperature: temperature - 0.1 }, { merge: true });
+    });
+  });
+
+  return timeMapper;
+};
+
+const scheduler = async (schedule) => {
+  const currentDay = getCurrentDay();
+  const currentTime = getCurrentTime();
+  // const boilerActive = await getBoilerActive();
+
+  const timeMap = timeMapper(schedule);
+  const todaysSchedule = timeMap[currentDay];
+
+  todaysSchedule.forEach(({ time, event }) => {
+    if (time === currentTime) {
+      setBoilerActive(event === 'on' ? true : false);
     }
   });
+};
