fa9f7dcf68d69a2618a961e68f4820fbd84c5eb6
Tal Koren <talkor@users.noreply.github.com>
2020-09-02 23:35:51 +0300

    add skeleton to notifications

---
+++ src/views/Home/NotificationsSkeleton.vue
@@ -0,0 +1,9 @@
+<template>
+  <section>
+    <article class="media" v-for="i in [1, 2, 3, 4, 5]" :key="i">
+      <div class="media-content">
+        <b-skeleton height="45px"></b-skeleton>
+      </div>
+    </article>
+  </section>
+</template>
