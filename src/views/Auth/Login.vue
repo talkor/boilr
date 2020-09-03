0b5e2efdb98626d0506d77e46c92978d65703c14
Tal Koren <talkor@users.noreply.github.com>
2020-09-04 01:49:56 +0300

    login screen

--- src/views/Auth/Login.vue
+++ src/views/Auth/Login.vue
@@ -1,40 +1,60 @@
 <template>
   <div class="sign-in">
-    <Title text="Welcome to iBoiler" />
+    <div class="logo-title">iBoiler</div>
+    <img class="logo-image" src="../../assets/logo-inverted.png" />
     <img class="login-image" src="../../assets/login.png" />
-    <GoogleSignInButton />
-    <!-- <EmailSignInButton />
-    <RegisterButton />-->
+    <GoogleSignInButton class="login-button" />
   </div>
 </template>
 
 <script>
 import GoogleSignInButton from '@/components/SignInButtons/GoogleSignInButton';
-// import EmailSignInButton from '@/components/SignInButtons/EmailSignInButton';
-// import RegisterButton from '@/components/SignInButtons/RegisterButton';
 import Title from '@/components/core/Title';
 
 export default {
   components: {
     GoogleSignInButton,
-    // EmailSignInButton,
-    // RegisterButton,
     Title
   }
 };
 </script>
 
 <style lang="scss" scoped>
+@import '@/styles/variables.scss';
+
 .sign-in {
   height: 100%;
   display: flex;
   justify-content: space-around;
   flex-direction: column;
   align-items: center;
+  position: relative;
+  background: $primary;
+  padding-block-end: 0px;
 
   .login-image {
-    padding-inline-start: 20px;
-    padding-inline-end: 20px;
+    padding-inline-start: 30px;
+    padding-inline-end: 30px;
+  }
+
+  .logo-image {
+    width: 100px;
+    position: absolute;
+    top: 105px;
+    left: 50px;
+  }
+
+  .logo-title {
+    color: white;
+    font-weight: 900;
+    font-size: 50px;
+    position: relative;
+    top: 60px;
+    left: 25px;
+  }
+
+  .login-button {
+    margin-block-end: 50px;
   }
 }
 </style>
